fce764851fa1fd101ee7f74c16b5a37d1c0cb31f
Artur Sushkov <a.sushkov@heyworks.com>
2017-08-09 13:15:07 +0300

    Update settings.

--- Trains/Assets/Scripts/Model/GameSettings.cs
+++ Trains/Assets/Scripts/Model/GameSettings.cs
@@ -5,12 +5,12 @@
 /// </summary>
 public static class GameSettings
 {
-    public static int fieldLength = 100;
+    public static int fieldLength = 80;
     public static int firstLineX = 1;
     public static int lineDeltaX = 2;
     public static int cargoEffect = 5;
     public static Dictionary<ActionObjectType, int> cooldowns = new Dictionary<ActionObjectType, int> { { ActionObjectType.Ambush, 45 }, { ActionObjectType.Arrow, 10 }, { ActionObjectType.Barrier, 30 } };
     public static Dictionary<ActionObjectType, int> installTime = new Dictionary<ActionObjectType, int> { { ActionObjectType.Ambush, 5 }, { ActionObjectType.Arrow, 0 }, { ActionObjectType.Barrier, 5 } };
     public static int teamInitTime = 20;
-    public static int safeZoneSize = 10;
+    public static int safeZoneSize = 7;
 }
